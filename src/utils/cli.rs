5994456ee24853ea5bce6ac4c0d6c6c1e231b319
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-07-26 19:00:06 +0800

    feat: setup run command

--- src/utils/cli.rs
+++ src/utils/cli.rs
@@ -5,16 +5,17 @@ use clap::{Args, Parser, Subcommand};
 #[clap(author, version, about,long_about=None)]
 pub struct Cli {
     #[clap(subcommand)]
-    command: CommandType,
+    pub command: CommandType,
 }
 
 #[derive(Debug, Subcommand)]
 pub enum CommandType {
+    /// Run a file
     Run(RunCommand),
 }
 
 #[derive(Debug, Args)]
 pub struct RunCommand {
     /// The file to run
-    file: std::path::PathBuf,
+    pub file: std::path::PathBuf,
 }
